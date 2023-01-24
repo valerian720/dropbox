a6eadb32f2eaffc8bda9ad7cac203d02ecfed554
Валериан Воротников <valerian7202@gmail.com>
2023-01-24 18:20:18 +0300

    init

---
+++ tool.py
@@ -0,0 +1,9 @@
+# get credits
+# get operation type
+# decide where is local path and where is dropbox path
+# check if file exists
+# try establish connection
+# try upload / download file
+# display downloading / uploading progress
+
+print("loading...")
