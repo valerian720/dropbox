8f413a7c296ffd4cb8d09f61d8c1a1eee6246204
Валериан Воротников <valerian7202@gmail.com>
2023-01-28 19:02:27 +0300

    finished prototypes

--- prototype/3.py
+++ prototype/3.py
@@ -7,25 +7,31 @@ token = open("secret/secret.token", "r").read()
 print(f"prototype 3: upload test2.txt")
 #
 source_folder = "downloaded/test2.txt"
-source_folder = "downloaded/train2.txt"
-target_folder = "/"
+# source_folder = "downloaded/train2.png"
+
+target_folder = "/new_folder/"
 #
 domain = "https://content.dropboxapi.com/"
-api_path = "2/files/download"
+api_path = "2/files/upload"
 
 url = f"{domain}{api_path}"
+target_filename = source_folder.split('/').pop()
 headers = {
     'Authorization': f'Bearer {token}',
-    'Dropbox-API-Arg': json.dumps({"path": source_folder}), 
+    'Dropbox-API-Arg': json.dumps({"autorename":False,"mode":"add","mute":False,"path":target_folder+target_filename,"strict_conflict":False}), 
+    'Content-Type': 'application/octet-stream',
 }
 
+file = open(source_folder,'rb')
 
-# curl -X POST https://content.dropboxapi.com/2/files/download \
+# curl -X POST https://content.dropboxapi.com/2/files/upload \
 #     --header "Authorization: Bearer <get access token>" \
-#     --header "Dropbox-API-Arg: {\"path\":\"/Homework/math/Prime_Numbers.txt\"}"
-# 
-req_result = requests.post(url, headers=headers)
+#     --header "Dropbox-API-Arg: {\"autorename\":false,\"mode\":\"add\",\"mute\":false,\"path\":\"/Homework/math/Matrices.txt\",\"strict_conflict\":false}" \
+#     --header "Content-Type: application/octet-stream" \
+#     --data-binary @local_file.txt
+
+
+req_result = requests.post(url, headers=headers, data=file)
 print(req_result.text)
 
-with open(f"{target_folder}{source_folder}", 'wb') as f:
-    f.write(req_result.content)
+# https://www.dropbox.com/developers/
--- prototype/4.py
+++ prototype/4.py
@@ -4,23 +4,27 @@ token = open("secret/secret.token", "r").read()
 # super secret
 
 print(f"prototype 4: get token from login and password")
-#
-# domain = "https://api.dropboxapi.com/"
-# api_path = "2/files/list_folder"
 
-# url = f"{domain}{api_path}"
-# headers = {
-#     'Authorization': f'Bearer {token}',
-#     'Content-Type': 'application/json',
-# }
+app_key = open("secret/app_key.token", "r").read()
+app_secret = open("secret/app_secret.token", "r").read()
 
-# body = {"include_deleted": False, "include_has_explicit_shared_members": False, "include_media_info": False,
-#         "include_mounted_folders": True, "include_non_downloadable_files": True, "path": "", "recursive": False}
+# build the authorization URL:
+authorization_url = "https://www.dropbox.com/oauth2/authorize?client_id=%s&response_type=code" % app_key
 
-# # curl -X POST https://api.dropboxapi.com/2/files/list_folder \
-# # --header "Authorization: Bearer <token>" \
-# # --header "Content-Type: application/json" \
-# # --data "{\"include_deleted\":false,\"include_has_explicit_shared_members\":false,\"include_media_info\":false,\"include_mounted_folders\":true,\"include_non_downloadable_files\":true,\"path\":\"/Homework/math\",\"recursive\":false}"
-# #
-# x = requests.post(url, headers=headers, json=body)
-# print(x.text)
+# send the user to the authorization URL:
+print('Go to the following URL and allow access:')
+print(authorization_url)
+
+# get the authorization code from the user:
+authorization_code = input('Enter the code:\n')
+
+# exchange the authorization code for an access token:
+token_url = "https://api.dropboxapi.com/oauth2/token"
+params = {
+    "code": authorization_code,
+    "grant_type": "authorization_code",
+    "client_id": app_key,
+    "client_secret": app_secret
+}
+r = requests.post(token_url, data=params)
+print(r.text)
